ebb98c8e1a2c00443976abf6866f4faf302fe503
pashag123 <pashag123@gmail.com>
2024-04-08 15:21:00 -0700

    working hard or hardly working

--- src/Resource.js
+++ src/Resource.js
@@ -1,7 +1,29 @@
 class Resources {
     constructor() {
+        //    everything planned to be downloaded
         this.toLoad = {
-            sky: "/sprites/sky.png"
+            sky: "/sprites/sky.png",
+            ground: "/sprites/ground.png",
+            hero: "/sprites/hero-sheet.png",
+            shadow: "/sprites/shadow.png",
         };
+        // a bucket to keep all images
+        this.images = {};
+
+        // load each image
+
+        Object.keys(this.toLoad).forEach(key => {
+            const img = new Image();
+            img.src = this.toLoad[key];
+            this.images[key] = {
+        image: img,
+        isLoaded: false
+            }
+            img.onload = () => {
+                this.images[key].isLoaded = true;
+            }
+        })
     }
 }
+// create one instance for whole app 
+export const resources = new Resources();
