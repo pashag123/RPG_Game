0e7a05c50fe8a766a8660be3e72c3abaec472912
pashag123 <pashag123@gmail.com>
2024-04-10 15:25:14 -0700

    were working on itgit add -Agit add -A

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css'
 import { resources } from "./src/Resource.js"
+import {Sprite} from "./src/Sprite.js";
+import {Vector2} from "./src/Vector2.js";
 
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
@@ -15,6 +17,15 @@ const draw = () => {
         ctx.drawImage(ground.image, 0, 0)
     }
 }
+
+
+const hero = new Sprite({
+    resource: resources.images.hero,
+    hFrames: 3,
+    vFrames: 8,
+    frame: 1
+})
+
 setInterval(() => {
 
     draw()
