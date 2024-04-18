d7655758d938022712d105b7e3eb1b80dec61006
pashag123 <pashag123@gmail.com>
2024-04-18 15:24:07 -0700

    progression

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 import './style.css'
 import { resources } from "./src/Resource.js"
-import {Sprite} from "./src/Sprite.js";
-import {Vector2} from "./src/Vector2.js";
+import { Sprite } from "./src/Sprite.js";
+import { Vector2 } from "./src/Vector2.js";
 
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
 
 const skySprite = new Sprite({
-    resource: resources.images.sky,
-    frameSize: new Vector2(320, 180)
-   })
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180)
+})
 
-   const groundSprite = new Sprite({
-    resource: resources.images.ground,
-    frameSize: new Vector2(320, 180)
-   })
+const groundSprite = new Sprite({
+  resource: resources.images.ground,
+  frameSize: new Vector2(320, 180)
+})
 
-   const hero = new Sprite ({
-    resource: resources.images.hero,
-    frameSize: new Vector2(32,32),
-    hFrames: 3,
-    vFrames: 8,
-    frame: 1
-   })
+const hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1
+})
 
-const shadow = new Sprite ({
-    resource: resources.images.shadow,
-    frameSize: new Vector2(32, 32)
+const shadow = new Sprite({
+  resource: resources.images.shadow,
+  frameSize: new Vector2(32, 32)
 })
 
 
@@ -34,14 +34,16 @@ const heroPos = new Vector2(16 * 6, 16 * 5);
 
 const draw = () => {
   skySprite.drawImage(ctx, 0, 0);
-  
+
   groundSprite.drawImage(ctx, 0, 0);
 
-//   center the Hero in the cell
+  //   center the Hero in the cell
   const heroOffset = new Vector2(-8, -21);
-  const heroPosX = heroPos.x+heroOffset.x;
-  const heroPosY = heroPos.y+1+heroOffset.y;
+  const heroPosX = heroPos.x + heroOffset.x;
+  const heroPosY = heroPos.y + 1 + heroOffset.y;
 
+
+  shadow.drawImage(ctx, heroPosX, heroPosY);
   hero.drawImage(ctx, heroPosX, heroPosY);
 }
 
@@ -49,6 +51,6 @@ const draw = () => {
 
 
 setInterval(() => {
-
-    draw()
+  
+  draw()
 }, 300)
---
+++ src/GameLoop.js
@@ -0,0 +1,40 @@
+export class GameLoop {
+    constructor(update, render) {
+
+        this.lastFrameTime = 0;
+        this.accumulatedTime = 0;
+        this.timeStep = 1000 / 60; // 60 frames persecond
+
+
+        this.update = update;
+        this.render = render;
+
+
+
+        this.rafid = null;
+        this.isRunning = false;
+    }
+
+    mainLoop = (timestamp) => {
+        if (!this.isRunning) return;
+
+        let deltaTime = timestamp - this.lastFrameTime;
+        this.lastFrameTime = timestamp;
+
+        // acumulate all the ti,e since the last frame
+        this.accumulatedTime += deltaTime;
+
+        // fixed time step updates
+        // if theres enough accumulated time to run one or more fixed update 
+        while (this.accumulatedTime >= this.timeStep) {
+            this.update(this.timeStep);
+            this.accumulatedTime -= this.timeStep;
+        }
+
+        this.render();
+
+
+        this.rafid = requestAnimationFrame(this.mainloop);
+
+    }
+}
